16c2e16f7513020f0f6ab8fbefcf27df1e5e90c9
coryATatum <103070281+coryATatum@users.noreply.github.com>
2022-06-24 22:45:11 -0500

    drop downs

--- dashboard.js
+++ dashboard.js
@@ -20,3 +20,19 @@ modal.addEventListener('click', function (e) {
     }
 });
 
+const dropDownBtn = document.querySelector('.editDropDown');
+const dropDown = document.querySelector('.dropdown-content');
+
+dropDownBtn.addEventListener('click', open);
+
+function open() {
+    if (dropDown.style.display === 'none') {
+        dropDown.style.display = 'block';
+    } else {
+        dropDown.style.display = 'none';
+    }
+};
+
+
+
+
