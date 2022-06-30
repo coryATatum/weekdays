7d94d43f83732e3000699d1211268050db465d8d
coryATatum <103070281+coryATatum@users.noreply.github.com>
2022-06-29 23:23:41 -0500

    edit/delete

--- dashboard.js
+++ dashboard.js
@@ -20,40 +20,7 @@ modal.addEventListener('click', function (e) {
     }
 });
 
-const dropDownBtn = document.getElementById('todo-editBtn');
-const dropDownBtn2 = document.getElementById('doing-editBtn');
-const dropDownBtn3 = document.getElementById('done-editBtn');
-const dropDown = document.getElementById('todo-dropdown');
-const dropDownTwo = document.getElementById('doing-dropdown');
-const dropDownThree = document.getElementById('done-dropdown');
 
-dropDownBtn.addEventListener('click', todoToggle);
-dropDownBtn2.addEventListener('click', doingToggle);
-dropDownBtn3.addEventListener('click', doneToggle);
-
-function todoToggle() {
-    if (dropDown.style.display === "none") {
-        dropDown.style.display = "block";
-    } else {
-        dropDown.style.display = "none";
-    }
-}
-
-function doingToggle() {
-    if (dropDownTwo.style.display === "none") {
-        dropDownTwo.style.display = "block";
-    } else {
-        dropDownTwo.style.display = "none";
-    }
-}
-
-function doneToggle() {
-    if (dropDownThree.style.display === "none") {
-        dropDownThree.style.display = "block";
-    } else {
-        dropDownThree.style.display = "none";
-    }
-}
 
 
 
