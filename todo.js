e31982c26a65cb006a1cc9202abb2833996daa58
coryATatum <tatumstew223@gmail.com>
2022-07-11 12:18:08 -0500

    drag and drop

--- todo.js
+++ todo.js
@@ -61,19 +61,19 @@ let createPost = () => {
     posts.innerHTML = "";
     data.map((x, y) => {
         return (posts.innerHTML += `
-        <div id=${y} class="task-card">
-        <div class="task-title">
-            <h3>${x.title}</h3>
-        </div>
-        <div class="task-desc"><span>${x.desciption}</span></div>
-        <div class="due-date"><strong>Due Date:</strong>${x.dueDate}
-            <div class="delete_editIcons">
-                <i onClick="editTask(this);" class=" fas fa-edit"></i>
-                <i onClick="deleteTask(this); createTasks();" id="discard"
-                    class="fas fa-trash-alt"></i>
-            </div>
-        </div>
-    </div>
+        <div id=${y} class="task-card" draggable="true">
+                                <div class="task-title">
+                                    <h3>${x.title}</h3>
+                                </div>
+                                <div class="task-desc"><span>${x.desciption}</span></div>
+                                <div class="due-date"><strong>Due Date:</strong>${x.dueDate}
+                                    <div class="delete_editIcons">
+                                        <i onClick="editTask(this);" class=" fas fa-edit"></i>
+                                        <i onClick="deleteTask(this); createTasks();" id="discard"
+                                            class="fas fa-trash-alt"></i>
+                                    </div>
+                                </div>
+                            </div>
                                 
     `);
     });
@@ -132,7 +132,47 @@ function discard() {
 
 })()
 
-//----------------------------END TO DO---------------------//
+//----------------------------Drag and Drop---------------------//
+
+const draggables = document.querySelectorAll('.task-card')
+const containers = document.querySelectorAll('.toDoCards')
+
+draggables.forEach(draggable => {
+    draggable.addEventListener('dragstart', () => {
+        draggable.classList.add('dragging')
+    })
+
+    draggable.addEventListener('dragend', () => {
+        draggable.classList.remove('dragging')
+    })
+})
+
+containers.forEach(container => {
+    container.addEventListener('dragover', e => {
+        e.preventDefault()
+        const afterElement = getDragAfterElement(container, e.clientY)
+        const draggable = document.querySelector('.dragging')
+        if (afterElement == null) {
+            container.appendChild(draggable)
+        } else {
+            container.insertBefore(draggable, afterElement)
+        }
+    })
+})
+
+function getDragAfterElement(container, y) {
+    const draggableElements = [...container.querySelectorAll('.draggable:not(.dragging)')]
+
+    return draggableElements.reduce((closest, child) => {
+        const box = child.getBoundingClientRect()
+        const offset = y - box.top - box.height / 2
+        if (offset < 0 && offset > closest.offset) {
+            return { offset: offset, element: child }
+        } else {
+            return closest
+        }
+    }, { offset: Number.NEGATIVE_INFINITY }).element
+}
 
 
 
