6c79f1407982a9cdeb5a5e0f72a5d1759147df8f
coryATatum <tatumstew223@gmail.com>
2022-07-26 21:52:55 -0500

    a little more edits

--- todo.js
+++ todo.js
@@ -289,6 +289,7 @@ var random_margin = ["-5px", "1px", "5px", "10px", "7px"];
 var random_colors = ["#c2ff3d", "#ff3de8", "#3dc2ff", "#04e022", "#bc83e6", "#ebb328"];
 var random_degree = ["rotate(3deg)", "rotate(1deg)", "rotate(-1deg)", "rotate(-3deg)", "rotate(-5deg)", "rotate(-8deg)"];
 var index = 0;
+let stickyModal = document.getElementById('stickyModal');
 
 
 window.onload = document.querySelector("#user_input").select();
@@ -306,6 +307,7 @@ document.querySelector("#user_input").addEventListener('keydown', (event) => {
     if (event.key === 'Enter') {
         const text = document.querySelector("#user_input");
         createStickyNote(text.value);
+        stickyModal.style.display = 'none'
     }
 });
 
